83a67cf7d47546e5c9eb19bf6178c7d872c423c4
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2025-06-19 00:07:26 -0500

    move files into correct folder

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {SearchService} from "../../services/search.service";
+import {SearchService} from "../../core/services/search.service";
 
 @Component({
   selector: 'app-navbar',
