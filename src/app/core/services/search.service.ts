b15975b84ce62afef8a703189c3e6c156ec4cd8d
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2025-06-22 17:31:14 -0500

    add navigation to results page

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -5,6 +5,7 @@ import {HttpClient} from "@angular/common/http";
 import { BehaviorSubject } from 'rxjs';
 import {API_ROUTES} from "@core/constants/routes.constants";
 import {Producto} from "@core/models/udea.model";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class SearchService {
   private productosSubject = new BehaviorSubject<Producto[]>([]);
   productos$ = this.productosSubject.asObservable();
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient, private router: Router) { }
 
   searchByName(query: string): void {
 
@@ -26,5 +27,7 @@ export class SearchService {
           console.error('Error fetching products:', error);
         }
       });
+
+    this.router.navigate(["/resultados"]);
   }
 }
