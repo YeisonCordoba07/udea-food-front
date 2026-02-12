beaa25c3b35da8646e34eb0777656b501eb5d316
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-02-11 21:18:28 -0500

    fetch categories in home page

--- src/app/components/molecules/category/category.component.ts
+++ src/app/components/molecules/category/category.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from '@core/models/udea.model';
+import { CategoriesService } from '@core/services/categories/categories.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+    selector: 'app-category',
+    templateUrl: './category.component.html',
+    styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+    categories$!: Observable<Categoria[]>;
 
-  ngOnInit(): void {
-  }
+    constructor(private categoriesService: CategoriesService) { }
+
+    ngOnInit(): void {
+        this.categories$ = this.categoriesService.getCategories();
+    }
 
 }
