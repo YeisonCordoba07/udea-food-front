45cc755df677f35bdc9617ee563e286df514d4bf
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-08-03 00:25:39 -0500

    add pipe async to show current page and total page in pagination.component

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { Filters, PageInfo } from '@core/models/udea.model';
+import { FiltersService } from '@core/services/filters/filters.service';
+import { SearchService } from '@core/services/search.service';
+import { map, Observable } from 'rxjs';
 
 
 @Component({
@@ -9,9 +13,23 @@ import { Component, inject, OnInit } from '@angular/core';
 export class PaginationComponent implements OnInit {
 
 
+    filtros$!: Observable<number>;
+    pageInfo$!: Observable<number>;
+
+    private filtroService = inject(FiltersService);
+    private searchService = inject(SearchService);
+
+    
   constructor() { }
 
   ngOnInit(): void {
+    this.filtros$ = this.filtroService.filters$.pipe(
+        map(f =>{
+            return f.page +1
+        })
+    );
+
+    this.pageInfo$ = this.searchService.pagination$.pipe(map(p=>p.totalPages));
   }
 
 
