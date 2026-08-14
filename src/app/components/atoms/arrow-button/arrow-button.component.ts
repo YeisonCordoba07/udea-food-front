03769865e496591f44893a69474d940c758572a5
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-08-14 13:57:19 -0500

    add secondary style to arrow-button

--- src/app/components/atoms/arrow-button/arrow-button.component.ts
+++ src/app/components/atoms/arrow-button/arrow-button.component.ts
@@ -6,7 +6,8 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
   styleUrls: ['./arrow-button.component.css']
 })
 export class ArrowButtonComponent implements OnInit {
-  @Input() direction: 'left' | 'right' = 'right';
+  @Input() direction: "left" | "right" = "right";
+  @Input() style: "primary" | "secondary" = "primary";
 
   @Output() onClick = new EventEmitter<void>();
   constructor() { }
